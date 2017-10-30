aedda7a43ca1cd3bce052fe740fa85b78f3e8342
xufeng1994 <914650562@qq.com>
2017-10-30 23:10:09 +0800

    整理了一下所有文件 新加了今天面试的解决方案

---
+++ CountArticle/index.js
@@ -0,0 +1,31 @@
+/*
+* auther: 赵增阳
+* Date 2017-10-30
+*/
+let fs = require('fs');
+
+let texts = fs.readFileSync('article.txt', 'utf8');
+
+// console.log(texts);  取值
+
+let allwords = {};
+let words = texts.split(" ");  //去除空格 -- 数组
+
+words.forEach(function (element) {
+    if (allwords[element] === undefined) {
+        allwords[element] = 0
+    }
+    allwords[element]++
+});
+// console.log(allwords) 存放到对象
+
+let maxCountWord = [];
+for(let word in allwords){
+    maxCountWord.push([word,allwords[word]]);
+}
+//  数组排序
+maxCountWord.sort(function(a,b){
+    return b[1] - a[1]
+})
+
+console.log(maxCountWord)
